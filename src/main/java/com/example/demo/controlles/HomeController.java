96b82e59ed564a52488ef25e522aa71649a4bbd3
Bogdan <krabog@gmail.com>
2019-06-28 16:57:04 +0300

    Homework

--- src/main/java/com/example/demo/controlles/HomeController.java
+++ src/main/java/com/example/demo/controlles/HomeController.java
@@ -9,6 +9,43 @@ public class HomeController {
 
     private String surname;
 
+    private String dateBirth;
+    private String placeBirth;
+    private String address;
+    private String email;
+
+    public String getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(String dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public String getPlaceBirth() {
+        return placeBirth;
+    }
+
+    public void setPlaceBirth(String placeBirth) {
+        this.placeBirth = placeBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getName() {
         return name;
     }
@@ -26,11 +63,21 @@ public class HomeController {
     }
 
     public String sayHello1() {
-        System.out.println("Hello2 " + name + " " + surname);
-        return "home.xhtml?faces-redirect=true&name=" + name;
+        System.out.println("Name & Surname :  " + name + " " + surname);
+        return "hello.xhtml?faces-redirect=true&name=" + name;
     }
 
     public void sayHello2() {
         System.out.println("Hello1 " + name + " " + surname);
+    }
+
+    public String submitBirth(){
+        System.out.println("Date and place of birth: " +dateBirth+" "+ placeBirth);
+        return "address.xhtml?faces-redirect=true&name="+ placeBirth;
+    }
+
+    public String submitAddress(){
+        System.out.println("Адрес: " + address);
+        return "email.xhtml?faces-redirect=true&name="+ address;
     }
 }
